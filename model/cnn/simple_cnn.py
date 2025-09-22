8580ecc4d9549d9554a0c111770815523244fb2f
Vitaly <vsg.sam@gmail.com>
2025-09-22 18:33:43 +0200

    reduce cnn model and added training results

--- model/cnn/simple_cnn.py
+++ model/cnn/simple_cnn.py
@@ -4,13 +4,13 @@ import torch.nn as nn
 class SimpleCNN(nn.Module):
     def __init__(self):
         super(SimpleCNN, self).__init__()
-        self.conv1 = nn.Conv2d(1, 16, kernel_size=3, stride=1, padding=1)
+        self.conv1 = nn.Conv2d(1, 2, kernel_size=3, stride=1, padding=1)
         self.relu1 = nn.ReLU()
         self.pool1 = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(2, 4, kernel_size=3, stride=1, padding=1)
         self.relu2 = nn.ReLU()
         self.pool2 = nn.MaxPool2d(2, 2)
-        self.fc1 = nn.Linear(32 * 7 * 7, 10)  # 10 output classes for MNIST
+        self.fc1 = nn.Linear(4 * 7 * 7, 10)  # 10 output classes for MNIST
 
     def forward(self, x):
         x = self.pool1(self.relu1(self.conv1(x)))
