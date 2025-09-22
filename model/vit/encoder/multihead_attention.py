e19c5c025002e3640516514a6bf6ebe445a29dd1
Vitaly <vsg.sam@gmail.com>
2025-09-22 16:23:51 +0200

    added multihead attention

---
+++ model/vit/encoder/multihead_attention.py
@@ -0,0 +1,20 @@
+import torch 
+import torch.nn as nn
+from model.vit.encoder.singlehead_attention import AttentionHead
+
+
+class AttentionMultiHead(nn.Module):
+    def __init__(self, hidden_dim, head_size, num_heads):
+        super().__init__()
+
+        self.heads = torch.nn.ModuleList(
+            [AttentionHead(hidden_dim, head_size) for _ in range(num_heads)]
+        )
+
+        self.dim_restoration = torch.nn.Linear(head_size * num_heads, hidden_dim)
+
+    def forward(self, input):
+        """ Result dimensionality is the same as input """
+        head_outputs = [head(input) for head in self.heads]
+        result = torch.cat(head_outputs, dim = -1)
+        return result
