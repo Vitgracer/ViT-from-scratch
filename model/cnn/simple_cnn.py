bug fix with softmax in vit + added same for cnn

model/cnn/simple_cnn.py
@@ -16,5 +16,5 @@ class SimpleCNN(nn.Module):
         x = self.pool1(self.relu1(self.conv1(x)))
         x = self.pool2(self.relu2(self.conv2(x)))
         x = x.view(x.size(0), -1)  # Flatten
-        x = self.fc1(x)
+        x = nn.Softmax(dim=-1)(self.fc1(x))
         return x

model/vit/simple_vit.py
@@ -17,7 +17,7 @@ class SimpleViT(nn.Module):
         ) 
 
         self.classifier = torch.nn.Sequential(
-            torch.nn.Linear(hidden_dim, 9),
+            torch.nn.Linear(hidden_dim, 10),
             nn.Softmax(dim=-1)
         )
 