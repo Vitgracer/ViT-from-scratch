14329c4ce77fd3ced2a763405c2af48c88e1fe17
Vitaly <vsg.sam@gmail.com>
2025-09-22 17:10:51 +0200

    added vit training

--- test/test_vit_parts.py
+++ test/test_vit_parts.py
@@ -8,6 +8,8 @@ from model.vit.embeddings.positional_encoder import PositionalEncoder
 from model.vit.encoder.singlehead_attention import AttentionHead
 from model.vit.encoder.multihead_attention import AttentionMultiHead
 from model.vit.encoder.vit_block import BlockViT
+from model.vit.simple_vit import SimpleViT
+
 
 embedder = PatchEmbedder(in_channels = 1, 
                          patch_size = 4, 
@@ -20,6 +22,16 @@ positionlal_encoder = PositionalEncoder(image_size = 28,
 attention = AttentionHead(8, 4)
 attention_multi = AttentionMultiHead(8, 4, 4)
 vit_block = BlockViT(8, 4, 4, 8)
+vit = SimpleViT(
+    in_channels=1, 
+    image_size=28, 
+    patch_size=4, 
+    hidden_dim=8,
+    num_layers=2,
+    head_size=4,
+    num_heads=4,
+    mlp_hidden_size=8
+)
 
 # image like MNIST
 input = torch.randn(1, 1, 28, 28)
@@ -28,3 +40,4 @@ positional_encoding = positionlal_encoder(embeddings)
 single_att = attention(positional_encoding)
 multi_att = attention_multi(positional_encoding)
 vit_block = vit_block(positional_encoding)
+vit_result = vit(input)
